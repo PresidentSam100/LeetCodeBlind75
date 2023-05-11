0f6cada2b328c84f7077f0e8f08442cedcd6c614
Sam Lu <pbcmarsh@gmail.com>
2023-05-11 17:06:18 -0500

    Add files via upload
    Folder for Arrays & Hashing

---
+++ Arrays & Hashing/ContainsDuplicate.java
@@ -0,0 +1,21 @@
+/*
+217. Contains Duplicate
+Code by: Sam Lu
+Language: Java
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int n = nums.length;
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            if (set.contains(num))
+                return true;
+            else
+                set.add(num);
+        }
+        return false;
+    }
+}
