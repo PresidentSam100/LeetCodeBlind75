0802a8cf79b4aea2318de96d9beec5e9d3f88074
Sam Lu <pbcmarsh@gmail.com>
2023-05-12 14:02:59 -0500

    Linked List Cycle Solution Java
    Linked List Cycle Solution Java

---
+++ Linked List/141-Linked-List-Cycle.java
@@ -0,0 +1,36 @@
+/*
+141. Linked List Cycle
+Link: https://leetcode.com/problems/linked-list-cycle/
+Code by: Sam Lu
+Language: Java
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return true;
+    }
+}
