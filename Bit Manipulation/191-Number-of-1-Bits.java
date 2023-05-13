a39466e855a20f79cd991870bd47b7dddc8ed8cf
Sam Lu <pbcmarsh@gmail.com>
2023-05-12 19:26:07 -0500

    Bit Manipulation Folder + Number of 1 Bits
    Bit Manipulation Folder + Number of 1 Bits

---
+++ Bit Manipulation/191-Number-of-1-Bits.java
@@ -0,0 +1,21 @@
+/*
+191. Number of 1 Bits
+Link: https://leetcode.com/problems/number-of-1-bits/
+Code by: Sam Lu
+Language: Java
+Time Complexity: O(1)
+Space Complexity: O(1)
+*/
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count  = 0;
+        while (n != 0) {
+            if ((n & 1) == 1) {
+                count++;
+            }
+            n >>>= 1;
+        }
+        return count;
+    }
+}
