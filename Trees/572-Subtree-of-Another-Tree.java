628548cf637ce34eb8c08177526164bbd0c1546c
Sam Lu <pbcmarsh@gmail.com>
2023-05-16 21:03:56 -0500

    Subtree of Another Tree Java Solution
    Subtree of Another Tree Java Solution

---
+++ Trees/572-Subtree-of-Another-Tree.java
@@ -0,0 +1,46 @@
+/*
+572. Subtree of Another Tree
+Link: https://leetcode.com/problems/subtree-of-another-tree/
+Code by: Sam Lu
+Language: Java
+Time Complexity: O(n*m)
+Space Complexity: O(n)
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null) {
+            return true;
+        } else if (root == null || subRoot == null) {
+            return false;
+        } else if (root.val == subRoot.val && isSameTree(root, subRoot)) {
+            return true;
+        } else {
+            return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);    
+        }    
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null && q != null || p != null && q == null) {
+            return false;
+        } else {
+            return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+}
