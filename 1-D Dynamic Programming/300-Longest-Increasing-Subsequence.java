06c195a20f13e9c6c699a13c1bd7778bd37ff8bb
Sam Lu <pbcmarsh@gmail.com>
2023-07-30 12:28:25 -0500

    Create 300-Longest-Increasing-Subsequence.java

---
+++ 1-D Dynamic Programming/300-Longest-Increasing-Subsequence.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int ans = 0;
+        int n = nums.length;
+        int[] sums = new int[n];
+        for (int i = 0; i < n; i++) {
+            int x = 0;
+            int y = ans;
+            while (x != y) {
+                int z = (x + y) / 2;
+                if (sums[z] < nums[i]) {
+                    x = z + 1;
+                } else {
+                    y = z;
+                }
+            }
+            sums[x] = nums[i];
+            if (x == ans) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
